a0e301d0d13dafca61aa665f048ee94cdedd8d0f
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-04-06 07:56:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.get('/api/getstats', (req, res) => {
-	async function fetchData() {
-		try {
-			const connection = mysql.createConnection({
-				host: process.env.DBServer,
-				user: process.env.DBUsername,
-				password: process.env.DBPassword,
-				database: process.env.DBName,
-				connectTimeout: 30000,
-				timeout: 30000,
-			});
+	try {
+		const connection = mysql.createPool({
+			host: process.env.DBServer,
+			user: process.env.DBUsername,
+			password: process.env.DBPassword,
+			database: process.env.DBName,
+			connectTimeout: 30000,
+            timeout: 30000,
+            waitForConnections: true,
+            connectionLimit: 100,
+            queueLimit: 0
+		});
 
-			const rows = connection.query(
-				'SELECT * FROM users ORDER BY date DESC',
-				(err, rows) => {
-					if (err) {
-						console.error(err);
-						return res.send(err);
-					}
-					return res.send(rows);
+		const rows = connection.query(
+			'SELECT * FROM users ORDER BY date DESC',
+			(err, rows) => {
+				if (err) {
+					console.error(err);
+					return res.send(err);
 				}
-			);
-		} catch (error) {
-			console.error(error);
-			return res.send(error);
-		}
+				return res.send(rows);
+			}
+		);
+	} catch (error) {
+		console.error(error);
+		return res.send(error);
 	}
-	fetchData();
 });
 
 app.listen(port, () => {
