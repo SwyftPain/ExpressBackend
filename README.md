cb1d736bf2f1febf8f59ae446d314ec6aef4b440
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-04-06 06:57:50 +0200

    t

--- README.md
+++
@@ -1,2 +0,0 @@
-# ExpressBackend
- Express Backend for GeoGuessr Stats
---
+++ dist/interfaces/global.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Error interface
+ */
+interface Error {
+    /**
+     * Error message
+     */
+    message: string;
+    /**
+     * Error status code
+     */
+    status: number;
+}
+export { Error };
