0e5209809fa2c34bfebd2029565dae13b9d5ea66
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-04-09 02:26:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
-const mysql = require( 'mysql' );
+const mysql = require('mysql');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello World!' });
-} );
+});
 
 app.use(
 	cors({
@@ -16,20 +16,20 @@ app.use(
 	})
 );
 
-app.get('/api/getstats', (req, res) => {
-	const connection = mysql.createPool({
-		host: process.env.DBServer,
-		user: process.env.DBUsername,
-		password: process.env.DBPassword,
-		database: process.env.DBName,
-		connectTimeout: 30000,
-        timeout: 30000,
-        connectionLimit: 60000,
-		queueLimit: 60000,
-		waitForConnections: true,
-		acquireTimeout: 30000,
-	});
+const connection = mysql.createPool({
+	host: process.env.DBServer,
+	user: process.env.DBUsername,
+	password: process.env.DBPassword,
+	database: process.env.DBName,
+	connectTimeout: 30000,
+	timeout: 30000,
+	connectionLimit: 60000,
+	queueLimit: 60000,
+	waitForConnections: true,
+	acquireTimeout: 30000,
+});
 
+app.get('/api/getstats', (req, res) => {
 	connection.query('SELECT * FROM users ORDER BY date DESC', (err, rows) => {
 		if (err) {
 			console.error(err);
@@ -37,6 +37,72 @@ app.get('/api/getstats', (req, res) => {
 		}
 		return res.send(rows);
 	});
+});
+
+app.post('/api/addstats/:userid/:winorloss', (req, res) => {
+	const userid = req.params.userid;
+	const winorloss = req.params.winorloss;
+	const time = new Date().getTime();
+
+	if (winorloss === 'win') {
+		connection.query(
+			'UPDATE users SET wins = wins + 1, time = ? WHERE id = ?',
+			[time, userid],
+			(err, rows) => {
+				if (err) {
+					console.error(err);
+					return res.send(err);
+				}
+				return res.send(rows);
+			}
+		);
+	} else {
+		connection.query(
+			'UPDATE users SET losses = losses + 1, time = ? WHERE id = ?',
+			[time, userid],
+			(err, rows) => {
+				if (err) {
+					console.error(err);
+					return res.send(err);
+				}
+				return res.send(rows);
+			}
+		);
+	}
+});
+
+app.post('/api/addnewstat/:userid/:username', (req, res) => {
+	const userid = req.params.userid;
+	const username = req.params.username;
+	const time = new Date().getTime();
+
+	connection.query(
+		'INSERT INTO users (discordid, user, wins, time) VALUES (?, ?, 0, ?)',
+		[userid, username, time],
+		(err, rows) => {
+			if (err) {
+				console.error(err);
+				return res.send(err);
+			}
+			return res.send(rows);
+		}
+	);
+});
+
+app.delete('/api/deletestats/:userid', (req, res) => {
+	const userid = req.params.userid;
+
+	connection.query(
+		'DELETE FROM users WHERE discordid = ?',
+		[userid],
+		(err, rows) => {
+			if (err) {
+				console.error(err);
+				return res.send(err);
+			}
+			return res.send(rows);
+		}
+	);
 });
 
 app.listen(port, () => {
